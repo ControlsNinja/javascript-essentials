35f92c52c9de3623433be25bad26d3ef51d2e3f7
ControlsNinja <jp@parsonscontrols.com>
2025-01-19 19:47:03 -0500

    adding reset to follower function

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -6,6 +6,12 @@ function increaseCount() {
   checkCountValue(); // Check count value and add display messages
 }
 
+function resetCount() {
+    count = 0; // Reset the count to 0
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and add display messages
+}
+
 function displayCount() {
 document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
 }
